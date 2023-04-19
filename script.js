e27cd3a7c9901bc64802424aea8080a7dfb74d49
ryan florez <ryanflorez@MotherShip.local>
2023-04-18 17:52:53 -0700

    Sending Work to laptop

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+
+
+//create array for lowerletter, upperletters, numbers, and special
+
 var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 var numbers = ['1','2','3','4','5','6','7','8','9']
 var special = ['!','@','#','$','%','^','&']
-
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-//create array for lowerletter, upperletters, numbers, and special
 
 //Make generatePassword function
 //Prompt for password length is a number
@@ -26,16 +25,23 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword(
-
-
-  );
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+
+  function generatePassword(){
+    // var length = prompt("How long would you like your password?")
+    var plength = JSON.parse(prompt("How long would you like your password?","0"));
+
+    if (plength.value<8 && plength.value>128){
+        console.log("Nice")
+    } else {
+        console.log(plength)
+    }
+  }
 
 }
 
